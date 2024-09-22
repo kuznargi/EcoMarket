dfccddaa9f66d9621d41a81ff3f6ea4571f56e8a
Nargiza <kuzybahovanargiza@gmail.com>
2024-09-22 13:44:44 +0500

    splash screen

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -27,12 +27,12 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     sourceSets {
-        
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.core.splashscreen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
